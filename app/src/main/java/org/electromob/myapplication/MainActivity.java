bfa27afe29c505c7951932a97d71aaa969f61cb8
madhuri <madhuriraj7878@gmail.com>
2020-12-01 12:02:27 +0530

    hello

---
+++ app/src/main/java/org/electromob/myapplication/MainActivity.java
@@ -0,0 +1,19 @@
+package org.electromob.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
